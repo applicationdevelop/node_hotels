33f3f64a286bf1c69bdb689498d3bb0a233e98ae
unknown <rahulkumar882535@gmail.com>
2025-07-09 11:15:32 +0530

    third version

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express=require('express')
 const app=express();
 const db=require('./db');
 
+//bodyparser used
 const bodyparser=require('body-parser');
 app.use(bodyparser.json());
 const personroutes=require('./routes/personcall');
